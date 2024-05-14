9c5fe3b0bce5f364c49c2eaaf23b3520a20aeb4f
BuildTools <unconfigured@null.spigotmc.org>
2024-05-14 12:09:37 -0400

    Finish v.1

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import { Route, Routes, Link } from 'react-router-dom'
+import Blue from './components/Blue'
+import Red from './components/Red'
+import Home from './components/Home'
+
+
+function App() {
+
+
+  return (
+    <>
+    <div id='container'>
+      <h1>Hello React Router!</h1>
+      <div id='navbar'>
+        <Link to="/red">Red</Link>
+        <Link to="/blue">Blue</Link>
+        <Link to="/">Home</Link>
+      </div>
+      <div id='main-section'>
+        <Routes>
+            <Route path='/blue' element={<Blue />}/>
+            <Route path='/red' element={<Red />} />
+            <Route path='/' element={<Home />}/>
+        </Routes>
+      </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
